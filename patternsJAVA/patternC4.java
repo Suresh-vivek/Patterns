26cdb2c507c5561e383f2cc87521c6b23ac6d5cd
AyuOrniThrONE <ayushprakashthaware@gmail.com>
2021-10-18 19:57:45 +0530

    Added

---
+++ patternsJAVA/patternC4.java
@@ -0,0 +1,16 @@
+/*
+Pattern is: 
+A B C D
+A B C D
+A B C D
+A B C D
+*/
+public class patternC4 {
+    public static void main(String[] args) {
+        int n=4;
+        for (int i = 0; i < n; i++) {
+            System.out.print("A B C D");
+            System.out.println();
+        }
+    }
+}
